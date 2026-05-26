631008e239b35ba6ae4db46157e89a1f180b6808
NimonokDanshi <aii.kmcryo@gmail.com>
2026-05-26 15:59:44 +0900

    ハイドレーション対応

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -1,4 +1,7 @@
-import Home from "@/app/page"
+"use client"
+
+import Link from "next/link" // Next.jsのLinkをインポート
+import { Home, ListTodo } from "lucide-react" // アイコンを正しくインポート
 import {
   Sidebar,
   SidebarContent,
@@ -11,38 +14,41 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import { Link } from "lucide-react"
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
-        <SidebarContent>
+      <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>アプリケーション</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
+              
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
-                  <Link href="#">
-                    <Home /> 
+                  {/* lucideのアイコンではなく、next/linkのLinkを使う */}
+                  <Link href="/">
+                    <Home className="size-4" /> 
                     <span>ダッシュボード</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
+
               <SidebarMenuItem>
                 <SidebarMenuButton asChild>
-                  <Link href="#">
-                    <Home /> 
+                  <Link href="/tasks">
+                    <ListTodo className="size-4" /> 
                     <span>タスク一覧</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
+
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-     <SidebarFooter />
+      <SidebarFooter />
     </Sidebar>
   )
 }
